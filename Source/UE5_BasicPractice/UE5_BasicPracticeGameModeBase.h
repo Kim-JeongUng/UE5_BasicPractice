33d584082c9a5dee7e2b2c6fd6e627ee5dc4c5fc
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2023-05-27 02:27:26 +0900

    Initial commit

---
+++ Source/UE5_BasicPractice/UE5_BasicPracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_BasicPracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_BASICPRACTICE_API AUE5_BasicPracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
